fb7012cb090946ffff3d891b3f5c9beb643c1a03
akillian@outlook.com <akillian@outlook.com>
2018-02-05 18:19:54 -0500

    commit

--- public/src/js/library/captionControl.js
+++ public/src/js/library/captionControl.js
@@ -1,16 +1,36 @@
 
 
-module.exports = () => {
+module.exports = (myChart) => {
+    var original = Chart.defaults.global.legend.onClick;
+    Chart.defaults.global.legend.onClick = function (e, legendItem) {
+        update_caption(legendItem);
+        original.call(this, e, legendItem);
+    };
     let iterator = myChart.data.datasets;
+    let labelObject = {};
+    let labels = {};
     iterator.forEach(function (arrayItem) {
         let label = arrayItem.label;
-        let labelObject = {
-            label: true
-        };
-
-        var labels = Object.assign(labels, labelObject);
+        labelObject[label] = true;
 
     });
-    console.log(arrayItem.label);
+
+    labels = Object.assign({}, labelObject);
+   
+
+    let caption = document.getElementById("caption");
+
+    let update_caption = function (legend) {
+        labels[legend.text] = legend.hidden;
+        console.log(labels[legend.text]);
+        let selected = Object.keys(labels).filter(function (key) {
+            return labels[key];
+        });
+
+        let text = selected.length ? selected.join(" & ") : "nothing";
+        console.log(text);
+        caption.innerHTML = "The chart is displaying " + text;
+    };
 
 };
+
